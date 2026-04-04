61f235c8c29b345474ec49574cc7e0de36d99e80
asasivlad13 <vbirisov@gmail.com>
2026-04-04 14:44:56 +0300

    init commit

---
+++ src/main/java/remote_pc_platform/RemotePcPlatformApplication.java
@@ -0,0 +1,13 @@
+package remote_pc_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotePcPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotePcPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/remote_pc_platform/RemotePcPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package remote_pc_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemotePcPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
